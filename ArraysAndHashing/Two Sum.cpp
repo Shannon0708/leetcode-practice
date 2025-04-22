d7379e69d3b9efe1797d705ef124f5966227150b
Shannon <m1111524@mail.ntust.edu.tw>
2025-04-23 02:58:43 +0800

    Feat: Solved 1. Two Sum

---
+++ ArraysAndHashing/Two Sum.cpp
@@ -0,0 +1,27 @@
+#include<vector>
+#include<unordered_map>
+#include<cstdio>
+
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> nums_map;
+    for(int i = 0; i< nums.size(); i++) {
+        int complement = target - nums[i];
+        if(nums_map.count(complement)) {
+            return {i ,nums_map[complement]};
+        }
+        nums_map[nums[i]] = i;
+    }
+    
+    return {};
+}
+
+int main() {
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = twoSum(nums, target);
+    for (int i = 0; i < nums.size(); i++) {
+        printf("%d ", nums[i]);
+    }
+    return 0;
+}
