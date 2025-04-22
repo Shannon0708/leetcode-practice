f8bbf1e5f56f6459269d881b7a2e9e16ce3b2c92
Shannon <m1111524@mail.ntust.edu.tw>
2025-04-23 03:52:23 +0800

    Fix: Correct return order in twoSum result

--- ArraysAndHashing/Two Sum.cpp
+++ ArraysAndHashing/Two Sum.cpp
@@ -2,17 +2,17 @@
 #include<unordered_map>
 #include<cstdio>
 
-
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 std::vector<int> twoSum(std::vector<int>& nums, int target) {
     std::unordered_map<int, int> nums_map;
     for(int i = 0; i< nums.size(); i++) {
         int complement = target - nums[i];
         if(nums_map.count(complement)) {
-            return {i ,nums_map[complement]};
+            return {nums_map[complement], i};
         }
         nums_map[nums[i]] = i;
     }
-    
     return {};
 }
 
