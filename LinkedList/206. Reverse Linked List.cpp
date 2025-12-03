89e09e42ab3f27b6af91ac6a159c921625be745d
Shannon <m1111524@mail.ntust.edu.tw>
2025-12-04 04:48:09 +0800

    Feat: Reverse Linked List with main func

--- LinkedList/206. Reverse Linked List.cpp
+++ LinkedList/206. Reverse Linked List.cpp
@@ -1,15 +1,17 @@
+#include <bits/stdc++.h>
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
 class Solution{
 public:
-    struct ListNode {
-        int val;
-        ListNode* next;
-        ListNode() : val(0), next(nullptr) {};
-        ListNode(int x) : val(x), next(nullptr) {};
-        ListNode(int x, ListNode* next) : val(x), next(next) {};
-    };
 
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = nullptr;
@@ -23,3 +25,26 @@ public:
         return prev;
     }
 };
+
+int main() {
+    // test 1->2->3->4->5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution sol;
+    ListNode* reverse = sol.reverseList(head);
+
+    ListNode* curr = reverse;
+    while(curr) {
+        cout << curr->val;
+        curr = curr->next;
+        if(curr) cout << "->";
+    }
+
+    cout << endl;
+    return 0;
+    
+}
